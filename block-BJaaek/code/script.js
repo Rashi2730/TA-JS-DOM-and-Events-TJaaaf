e74c96a65d4dbdb5fc2ddaf74b0e550bd18423be
Rashi Israni <rashiisrani97@gmail.com>
2022-02-07 06:38:10 +0530

    colors

---
+++ block-BJaaek/code/script.js
@@ -0,0 +1,16 @@
+let box1 = document.querySelector(".first");
+let box2 = document.querySelector(".second");
+
+
+
+
+
+box1.addEventListener("click",function(){
+    box1.style.backgroundColor = "blue";
+} );
+
+
+
+box2.addEventListener("mousemove",function(){
+    box2.style.backgroundColor = "#EBFDF1";
+} );
