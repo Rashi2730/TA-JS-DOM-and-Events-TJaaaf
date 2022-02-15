7e6cf1756eb67ee2bf260ad2de9b4adbe760156f
Rashi Israni <rashiisrani97@gmail.com>
2022-02-15 10:05:05 +0530

    Forms

---
+++ block-BJaaer/code/script.js
@@ -0,0 +1,17 @@
+let form = document.querySelector(`.container`);
+
+let name , email , movie, color, rating, genre;
+let agree = 👉 You agree to the 
+
+function handleForm(event){
+    event.preventDefault();
+
+}
+
+
+
+
+
+
+
+form.addEventListener(submit, handleForm);
